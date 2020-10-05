b11bb9edaf9669a1ec77b01160d8ae7d4f394203
Seulki <67934680+multivision-sk@users.noreply.github.com>
2020-10-05 14:16:11 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css' ; 
+import {HashRouter,Route} from 'react-router-dom' ; 
+import About from './route/About' ; 
+import Home from './route/Home' ; 
+import Navigation from './component/Navigation' ; 
+import Detail from './route/Detail' ; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <Navigation /> 
+      <Route path="/about" component={About} />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/movie-detail" component={Detail} />
+          </HashRouter>
+  ) 
 }
 
-export default App;
+export default App ; 
+
---
+++ src/route/About.js
@@ -0,0 +1,14 @@
+import React from 'react' ; 
+import './About.css';
+
+function About(props) {
+    console.log(props) ; 
+    return (
+        <div className="about__container">
+    <span>Movies make us be more emotional.</span> 
+    </div>
+    );  
+}
+
+export default About ; 
+
